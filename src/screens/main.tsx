6fcdd237e27763201c81cd9831e5e81aae691c42
Pavanarathod <edrocullen13@gmail.com>
2022-05-11 17:50:14 +0530

    makding changes

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import { Text, Box, Center, VStack, useColorModeValue } from "native-base";
 import ThemeToggle from "../components/theme-toggle";
+import AnimatedCheckbox from "react-native-checkbox-reanimated";
 
 const MainScreen = () => {
+  const [checked, setChecked] = React.useState<boolean>(false);
   return (
     <Center
       _dark={{ bg: "blueGray.900" }}
@@ -11,6 +13,14 @@ const MainScreen = () => {
       flex={1}
     >
       <VStack space={5} alignItems="center">
+        <Box w="100px" h="100px">
+          <AnimatedCheckbox
+            checked={checked}
+            highlightColor="#4444ff"
+            checkmarkColor="#ffffff"
+            boxOutlineColor="#4444ff"
+          />
+        </Box>
         <Box p={10} bg={useColorModeValue("red.500", "yellow.500")}>
           <Text>Hello World</Text>
         </Box>
