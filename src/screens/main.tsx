88c2fb7526912c3baae961500e3259342bded103
Pavanarathod <edrocullen13@gmail.com>
2022-05-11 15:16:46 +0530

    initial setup and building dark mode theme

---
+++ src/screens/main.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { Text, Box, Center, VStack, useColorModeValue } from "native-base";
+import ThemeToggle from "../components/theme-toggle";
+
+const MainScreen = () => {
+  return (
+    <Center
+      _dark={{ bg: "blueGray.900" }}
+      _light={{ bg: "blueGray.50" }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <Box p={10} bg={useColorModeValue("red.500", "yellow.500")}>
+          <Text>Hello World</Text>
+        </Box>
+        <ThemeToggle />
+      </VStack>
+    </Center>
+  );
+};
+
+export default MainScreen;
